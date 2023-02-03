4f619f316a76454b3d337b9aa64b5059fa2fe314
laoderidho <2081007@unai.edu>
2023-02-03 21:48:08 +0700

    update navbar
    navbar is not perfect

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Home from "./components/Screen/Home";
+import Sidebar from "./components/components/Sidebar";
 
 const App = ()=>{
 
   return(
     <BrowserRouter>
         <Routes>
-            <Route path="/" element={<Home/>}/>
+            <Route path="/" element={<Sidebar/>}/>
         </Routes>
     </BrowserRouter>
   )
--- client/src/components/Screen/Home.js
+++ client/src/components/Screen/Home.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Sidebar from "../components/Sidebar";
+
 
 const Home = () => {
   return (
     <div>
-        
+        <h1>This Home</h1>
     </div>
   );
 };

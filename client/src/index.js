9617de858f6f2ec4859e5e606ef64c142fa1d28e
laoderidho <2081007@unai.edu>
2023-02-03 21:03:01 +0700

    update today

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import '@fortawesome/fontawesome-free/css/all.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>

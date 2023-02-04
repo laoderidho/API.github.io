7210420d7ccbc2123a057a3441fb3e072f36a012
laoderidho <2081007@unai.edu>
2023-02-04 15:51:09 +0700

    update now

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,20 @@
 import React from "react";
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from "./components/Screen/Home"
+import About from './components/Screen/About'
 import Sidebar from "./components/components/Sidebar";
 
 const App = ()=>{
 
   return(
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Sidebar/>}/>
-        </Routes>
-    </BrowserRouter>
+    <>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/Home" element={<Home/>}/>
+                <Route path="/about" element={<About/>} />
+            </Routes>
+        </BrowserRouter>
+    </>
   )
 }
 
--- client/src/components/Screen/Home.js
+++ client/src/components/Screen/Home.js
@@ -4,7 +4,7 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-        <h1>This Home</h1>
+       <h1>Hello</h1>
     </div>
   );
 };

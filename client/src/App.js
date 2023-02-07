58495b9df4aa7a0962b682410689f116e616739e
laoderidho <2081007@unai.edu>
2023-02-07 09:46:37 +0700

    update getData

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from "./components/Screen/Home"
 import About from './components/Screen/About'
-import Sidebar from "./components/components/Sidebar";
 
 const App = ()=>{
 
@@ -10,7 +9,7 @@ const App = ()=>{
     <>
         <BrowserRouter>
             <Routes>
-                <Route path="/Home" element={<Home/>}/>
+                <Route path="/" element={<Home/>}/>
                 <Route path="/about" element={<About/>} />
             </Routes>
         </BrowserRouter>
--- client/src/components/Screen/Home.js
+++ client/src/components/Screen/Home.js
@@ -1,10 +1,38 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import axios from 'axios'
 
 const Home = () => {
+
+  useEffect(()=>{
+
+  }, []);
+
+  const [data, setData] = useState([]);
+
+  const getData = async ()=>{
+    const res = await axios.get('')
+  }
+
   return (
-    <div>
-       <h1>Hello</h1>
+    <div className="container" >
+       <table class="table table-striped">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Nama</th>
+              <th>NIM</th>
+              <th>Jurusan</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th></th>
+              <th></th>
+              <th></th>
+              <th></th>
+            </tr>
+          </tbody>
+       </table>
     </div>
   );
 };
